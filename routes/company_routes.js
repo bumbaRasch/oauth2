7a77aef5ff8c4b210b576bf90b794b70b627175b
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-24 13:17:10 +0100

    Add authentication and authorization middleware to PUT and DELETE routes in company_routes.js

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -1,3 +1,4 @@
+// routes/company_routes.js
 import express from 'express';
 import company_controller from '../controllers/company/company_controller.js';
 import { company_middlewares } from '../middlewares/company_middleware.js';
@@ -7,8 +8,8 @@ import { auth_middleware } from '../middlewares/auth_middleware.js';
 const router = express.Router();
 
 router.post('/companies', validation_middleware.validate_registration_input, company_controller.create);
-router.get('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.get_company);
-router.put('/companies/:uuid', company_middlewares.company_exists,  company_controller.update); //  auth_middleware.authenticate_user,
-router.delete('/companies/:uuid', company_controller.delete);
+router.get('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.get_company);
+router.put('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.update);
+router.delete('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authorize_company, company_middlewares.company_exists, company_controller.delete);
 
 export default router;
