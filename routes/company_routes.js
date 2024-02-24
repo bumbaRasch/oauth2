aead609915310c8054a92fbe9210120514c3bf61
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-24 21:59:31 +0100

    Update company routes: restrict delete company to admin role

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -10,7 +10,7 @@ const router = express.Router();
 router.post('/companies', validation_middleware.validate_registration_input, company_controller.create);
 router.get('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.get_company);
 router.put('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.update);
-router.delete('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists, company_controller.delete);
+router.delete('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authorize_roles(['admin']), auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists, company_controller.delete);
 
 router.get('/companies/:uuid/users', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_controller.get_company_users);
 router.get('/companies/:uuid/users/search', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_controller.search_company_users);
