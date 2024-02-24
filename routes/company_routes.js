f16bad1d37f8ebef81eab7baa44ae71a047090bd
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-24 13:31:54 +0100

    Add authenticate_company middleware to DELETE route in company_routes.js

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -10,6 +10,6 @@ const router = express.Router();
 router.post('/companies', validation_middleware.validate_registration_input, company_controller.create);
 router.get('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.get_company);
 router.put('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.update);
-router.delete('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authorize_company, company_middlewares.company_exists, company_controller.delete);
+router.delete('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists, company_controller.delete);
 
 export default router;
