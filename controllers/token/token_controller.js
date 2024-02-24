6e09bf76aa19f9648e68db01d38e70c7eb866100
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-24 13:10:44 +0100

    Refactor token_controller to use company_uuid instead of companyUuid

--- controllers/token/token_controller.js
+++ controllers/token/token_controller.js
@@ -4,17 +4,17 @@ import User from '../../models/User.js';
 
 export const token_controller = {
     generate_token: async (req, res) => {
-        const { uuid, companyUuid } = req.body;
+        const { uuid, company_uuid } = req.body;
 
         try {
-            const user = await User.findOne({ where: { uuid: uuid, company_id: companyUuid } });
+            const user = await User.findOne({ where: { uuid: uuid, company_id: company_uuid } });
 
             if (!user) {
                 return res.status(400).json({ error: 'No user found with that UUID and Company UUID' });
             }
 
-            const token = jwt.sign({ uuid: user.uuid, companyUuid: user.company_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-            const refreshToken = jwt.sign({ uuid: user.uuid, companyUuid: user.company_id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+            const token = jwt.sign({ uuid: user.uuid, company_uuid: user.company_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            const refreshToken = jwt.sign({ uuid: user.uuid, company_uuid: user.company_id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
 
             res.json({ token, refreshToken });
         } 
@@ -24,16 +24,16 @@ export const token_controller = {
     },
 
     refresh_token: async (req, res) => {
-        const { uuid, companyUuid } = req;
+        const { uuid, company_uuid } = req;
 
         try {
-            const user = await User.findOne({ where: { uuid: uuid, company_id: companyUuid } });
+            const user = await User.findOne({ where: { uuid: uuid, company_id: company_uuid } });
 
             if (!user) {
                 return res.status(403).json({ error: 'Access is forbidden' });
             }
 
-            const newToken = jwt.sign({ uuid: user.uuid, companyUuid: user.company_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            const newToken = jwt.sign({ uuid: user.uuid, company_uuid: user.company_id }, process.env.JWT_SECRET, { expiresIn: '1h' });
             return res.json({ token: newToken });
         } 
         catch (err) {
