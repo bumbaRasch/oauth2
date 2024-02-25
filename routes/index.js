50981ad702741a8cc04516430796792300263602
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-25 16:24:15 +0100

    Add auth routes to main router. Integrated the authentication routes into the main router to handle OAuth 2.0 authorization flow.

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,14 @@ import user_routes from './user_routes.js';
 import error_routes from './error_routes.js';
 import client_routes from './client_routes.js';
 import company_routes from './company_routes.js';
+import auth_routes from './auth_routes.js';
 
 router.use(user_routes);
 // router.use('/', error_routes);
 router.use('/protected', error_routes);
 router.use(client_routes);
 router.use(company_routes);
+router.use(auth_routes);
 
 
 export default router;
