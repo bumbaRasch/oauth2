9f584f35a3751de3b6035d862902ff5b2c4c8c7e
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-25 16:25:56 +0100

    Add auth routes to main router.

---
+++ routes/auth_routes.js
@@ -0,0 +1,12 @@
+// auth_routes.js
+import express from 'express';
+import { auth_controller } from '../controllers/auth_controller.js';
+import { user_controller } from '../controllers/user_controller.js';
+import { token_controller } from '../controllers/token/token_controller.js'; // import the token controller
+
+const router = express.Router();
+
+
+router.get('/authorize', auth_controller.authorize);
+
+export default router;
