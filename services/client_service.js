3559d4578fed60842b8c99582db7906fa5611034
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-26 23:12:30 +0100

    Create client_service.js. This service handles the business logic for client operations, including creating, retrieving, updating, and deleting clients. It also checks for the existence of the company before creating a client.

---
+++ services/client_service.js
@@ -0,0 +1,58 @@
+// services/client_service.js
+import Client from "../models/Client.js";
+import Company from "../models/Company.js";
+import crypto from 'crypto';
+import company_service_helpers from "../helpers/company_service_helpers.js";
+
+export const client_service = {
+    create_client: async (company_id, name, redirect_uri, grant_types, scope, active) => {
+        // Check if company_id is provided
+        if (!company_id) {
+            throw new Error('company_id is required');
+        }
+        // Check if the company exists
+        const company = await Company.findByPk(company_id);
+        if (!company) {
+            throw new Error('Company not found');
+        }
+
+        const password =  crypto.randomBytes(16).toString('hex')
+        const client_secret = await company_service_helpers.hash_password(password);
+        const client = await Client.create({ client_secret, company_id, name, redirect_uri, grant_types, scope, active });
+        await client.save();
+        return { client_id: client.client_id, client_secret: client.client_secret, company_id: client.company_id, name: client.name, redirect_uri: client.redirect_uri, grant_types: client.grant_types, scope: client.scope, active: client.active };
+    },
+
+    get_all_clients: async () => {
+        const clients = await Client.findAll();
+        return clients;
+    },
+
+    get_one_client: async (client_id) => {
+        const client = await Client.findOne({ where: { client_id } });
+        if (!client) {
+            throw new Error('Client not found');
+        }
+        return client;
+    },
+
+    update_client: async (client_id, updates) => {
+        console.log('updates', updates);
+        const client = await Client.findOne({ where: { client_id } });
+        if (!client) {
+            throw new Error('Client not found');
+        }
+        await client.update(updates);
+        return client;
+    },
+
+    delete_client: async (client_id) => {
+        const client = await Client.findOne({ where: { client_id } });
+        if (!client) {
+            throw new Error('Client not found');
+        }
+        await client.destroy();
+    }
+};
+    
+    
