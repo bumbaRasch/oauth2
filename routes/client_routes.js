b39089613b0619dbf44b93194d64705686f9e13f
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-26 23:11:41 +0100

    Refactor client_routes.js. Updated routes to match new methods in client_controller. Added new routes for getting all clients, updating a client, and deleting a client.

--- routes/client_routes.js
+++ routes/client_routes.js
@@ -1,10 +1,14 @@
 // routes/client_routes.js
 import express from 'express';
+import { validation_middleware } from '../middlewares/validation_middleware.js';
 import {client_controller} from '../controllers/client_controller.js';
 
 const router = express.Router();
 
-router.get('/client/:clientId', client_controller.get_client);
-router.post('/register-client', client_controller.register_client);
+router.post('/clients', client_controller.create_client);
+router.get('/clients', client_controller.get_all_clients);
+router.get('/clients/:client_id', client_controller.get_one_client);
+router.put('/clients/:client_id', client_controller.update_client);
+router.delete('/clients/:client_id', client_controller.delete_client);
 
 export default router;
