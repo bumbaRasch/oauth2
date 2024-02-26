9c6dbdd4b841ea4b1d9f92ae49fbf5eb3976e40f
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-26 23:10:39 +0100

    Refactor client_controller.js. Moved business logic to client_service.js for better separation of concerns. Added new methods for getting all clients, updating a client, and deleting a client. Improved error handling.

--- controllers/client_controller.js
+++ controllers/client_controller.js
@@ -1,57 +1,63 @@
 // controllers/client_controller.js
 import Client from '../models/Client.js';
 import crypto from 'crypto';
+import { client_service } from '../services/client_service.js';
 
 
 export const client_controller = {
-    register_client: async (req, res) => {
-        const { redirect_uri, grant_types, scope, user_id } = req.body;
-        
-        /*
-        // Validate the request
-        {
-            "redirect_uri": "http://myapp.com/callback",
-            "grant_types": ["authorization_code", "refresh_token"],
-            "scope": ["read", "write"],
-            "user_id": "123"
-        }
-
-        */
-
-        // Generate a new client_id and client_secret
-        const client_id = crypto.randomBytes(20).toString('hex');
-        const client_secret = crypto.randomBytes(40).toString('hex');
-
+    create_client: async (req, res) => {
         try {
-            const client = await Client.create({
-                client_id,
-                client_secret,
-                redirect_uri,
-                grant_types,
-                scope,
-                user_id,
-                active: true
-            });
-
-            res.status(201).json({
-                message: 'Client registered successfully',
-                client_id: client.client_id,
-                client_secret: client.client_secret,
-            });
-        } catch (error) {
-            console.log(error);
-            res.status(400).send({ error: 'Error registering client' });
+            const { company_id, name, redirect_uri, grant_types, scope, active } = req.body;
+            const client = await client_service.create_client(company_id, name, redirect_uri, grant_types, scope, active);
+            res.status(201).json(client);
+        } 
+        catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Error occurred while creating the client.', error: error.message, stack: error.stack });
         }
     },
-    get_client: async (req, res) => {
-        const { clientId } = req.params;
-        
-        const client = await Client.findOne({ where: { client_id: clientId } });
 
-        if (!client) {
-            return res.status(404).send({ error: 'Client not found' });
+    get_one_client: async (req, res) => {
+        try {
+            const { client_id } = req.params;
+            const client = await client_service.get_one_client(client_id);
+            res.status(200).json(client);
+        } catch (error) {
+            res.status(500).json({ message: 'Error occurred while retrieving the client.' });
         }
+    },
 
-        res.send({ clientId: client.client_id, clientSecret: client.client_secret });
-    }
+    get_all_clients: async (req, res) => {
+        try {
+            const clients = await client_service.get_all_clients();
+            res.status(200).json(clients);
+        } 
+        catch (error) {
+            res.status(500).json({ message: 'Error occurred while retrieving clients.' });
+        }
+    },
+    
+    update_client: async (req, res) => {
+        try {
+            const { client_id } = req.params;
+            const updates = req.body;
+            const client = await client_service.update_client(client_id, updates);
+            res.status(200).json(client);
+        } 
+        catch (error) {
+            console.error(error); // log the error
+            res.status(500).json({ message: 'Error occurred while updating the client.' });
+        }
+    },
+
+    delete_client: async (req, res) => {
+        try {
+            const { client_id } = req.params;
+            await client_service.delete_client(client_id);
+            res.status(204).send();
+        } 
+        catch (error) {
+            res.status(500).json({ message: 'Error occurred while deleting the client.' });
+        }
+    },
 };
