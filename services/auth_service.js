5549fca89c0d309c384a2b6200a8e0baf7e09fd6
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-27 21:15:01 +0100

    Remove is_admin function from auth_service

--- services/auth_service.js
+++ services/auth_service.js
@@ -4,14 +4,7 @@ import Company from '../models/Company.js';
 import User from '../models/User.js';
 
 export const auth_service = {
-    is_admin: async (userUuid) => {
-        const user = await User.findByPk(userUuid);
-        if (!user) {
-            throw new Error('User not found');
-        }
-        return user.role === 'admin';
-    },
-
+    
     is_company_owner: async (userUuid, companyUuid) => {
         const user = await User.findByPk(userUuid);
                
