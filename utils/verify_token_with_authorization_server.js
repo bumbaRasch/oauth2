830f0af65a5e4c9665ccae95b9e1a1c7909a3a8f
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-28 23:19:53 +0100

    Added verify_token_with_authorization_server function and axios dependency

---
+++ utils/verify_token_with_authorization_server.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const verify_token_with_authorization_server = async (token, client_id, client_secret) => {
+    try {
+        const response = await axios({
+            method: 'post',
+            url: 'http://localhost:3000/oauth/check_token',
+            headers: {
+                'Authorization': 'Basic ' + Buffer.from(`${client_id}:${client_secret}`).toString('base64')
+            },
+            data: {
+                token: token
+            }
+        });
+
+        return response.data.active;
+    } catch (error) {
+        console.error(error);
+        return false;
+    }
+};
