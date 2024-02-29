98c2143774e4eb95b5c308e046f92a825805f653
Dimitri Pisarev <info@dimitripisarev.com>
2024-02-29 23:40:32 +0100

    Refactor generate_token function in token_service to accept authorization code instead of uuid and company_uuid. Add authorization code and user lookup in generate_token function. Update generate_token call in router to pass authorization code.

--- services/token/token_service.js
+++ services/token/token_service.js
@@ -1,6 +1,7 @@
 // services/token_service.js
 import jwt from 'jsonwebtoken';
-import User from '../../models/User.js';
+import Client from '../../models/User.js';
+import AuthorizationCode from '../../models/AuthorizationCode.js';
 
 
 export const token_service = {
@@ -14,21 +15,25 @@ export const token_service = {
         }
     },
 
-    generate_token: async (uuid, company_uuid) => {
-        const user = await User.findOne({ where: { uuid: uuid, company_id: company_uuid } });
-
-        if (!user) {
-            throw new Error('No user found with that UUID and Company UUID');
+    generate_token: async (code) => {
+        const auth_code = await AuthorizationCode.findOne({ where: { authorization_code: code } });
+        if (!auth_code) {
+            throw new Error('Invalid authorization code');
         }
-
+    
+        const user = await Client.findByPk(auth_code.user_id);
+        if (!user) {
+            throw new Error('No user found with that ID');
+        }
+    
         const token        = jwt.sign({ uuid: user.uuid, company_uuid: user.company_id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_TTL || '1h' })
         const refreshToken = jwt.sign({ uuid: user.uuid, company_uuid: user.company_id }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
-
+    
         return { token, refreshToken };
     },
 
     refresh_token: async (uuid, company_uuid) => {
-        const user = await User.findOne({ where: { uuid: uuid, company_id: company_uuid } });
+        const user = await Client.findOne({ where: { uuid: uuid, company_id: company_uuid } });
 
         if (!user) {
             throw new Error('Access is forbidden');
