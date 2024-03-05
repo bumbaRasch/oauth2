03c5faca64baa8d19581028d32eeaa80e07474e8
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-05 01:40:02 +0100

    Add company registration route

--- routes/company_routes.js
+++ routes/company_routes.js
@@ -9,7 +9,11 @@ import { user_middleware } from '../middlewares/user_middleware.js';
 
 const router = express.Router();
 
-router.post('/companies', validation_middleware.validate_registration_input, company_controller.create);
+router.get('/register/company', function(req, res) {
+    res.render('register_company');
+});
+
+router.post('/register/company', validation_middleware.validate_registration_input, company_controller.create);
 router.get('/companies', auth_middleware.authenticate_user, auth_middleware.authorize_roles(['admin']), company_controller.get_companies);
 router.get('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.get_company);
 router.put('/companies/:uuid', auth_middleware.authenticate_user, auth_middleware.authorize_roles(['admin']), auth_middleware.authorize_permissions(['update_company']), auth_middleware.authenticate_company, auth_middleware.authorize_company, company_middlewares.company_exists,  company_controller.update);
