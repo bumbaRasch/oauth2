0414752377b6b3a9a8899aa85aacdd9707406060
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-05 01:39:26 +0100

    Add home router to index routes

---
+++ routes/home_router.js
@@ -0,0 +1,11 @@
+// routes/company_routes.js
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('Welcome to the home page!');
+});
+
+
+export default router;
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ import error_routes from './error_routes.js';
 import client_routes from './client_routes.js';
 import company_routes from './company_routes.js';
 import auth_routes from './auth_routes.js';
+import home_router from './home_router.js';
 
 router.use(user_routes);
 // router.use('/', error_routes);
@@ -15,6 +16,6 @@ router.use('/protected', error_routes);
 router.use(client_routes);
 router.use(company_routes);
 router.use(auth_routes); 
-
+router.use(home_router);
 
 export default router;
