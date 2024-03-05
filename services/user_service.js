266b2f6c823f6ecd6463e1a3ee568d4334efe24f
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-05 01:36:37 +0100

    Update login function in user service (email)

--- services/user_service.js
+++ services/user_service.js
@@ -20,7 +20,7 @@ export const user_service = {
   }, 
 
   login: async ( body ) => {
-    const  { username, password, company_id } = body;
+    const  { username, password, email, company_id } = body;
     const user = await User.findOne({ where: sequelize.and({ username: username }, { company_id: company_id } )});
     if (!user) {
       throw new Error('Invalid username, password, or company');
