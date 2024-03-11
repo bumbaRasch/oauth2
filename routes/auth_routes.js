063be2fc064c7f4c80a516c8c0c76d50bbc2868d
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-11 23:27:21 +0100

    Add authentication middleware to token routes: Added authentication middleware to the token routes in auth_routes.js for added security. This ensures that the user is authenticated before they can exchange a code for a token, check a token, or refresh a token.

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -10,8 +10,8 @@ import { auth_middleware } from '../middlewares/auth_middleware.js';
 const router = express.Router();
 
 router.get('/oidc/authorize', auth_middleware.authenticate, auth_controller.authorize);
-router.post('/oidc/token', token_controller.exchange_code_for_token);
-router.post('/oidc/check_token',token_controller.check_token);
-router.post('/oidc/refresh_token', token_controller.refresh_token);
+router.post('/oidc/token', auth_middleware.authenticate, token_controller.exchange_code_for_token);
+router.post('/oidc/check_token', auth_middleware.authenticate, token_controller.check_token);
+router.post('/oidc/refresh_token', auth_middleware.authenticate, token_controller.refresh_token);
 
 export default router;
