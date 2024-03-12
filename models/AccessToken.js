e9403fba20a1d5c9a4ab433896b67f6538cb7935
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-12 22:15:25 +0100

    Update AccessToken model. Change access_token_id type from BIGINT to UUID and add default value UUIDV4.

--- models/AccessToken.js
+++ models/AccessToken.js
@@ -2,18 +2,15 @@
 import { Model, DataTypes } from 'sequelize';
 import sequelize from './sequelize.js'; // import your sequelize instance
 
-
 class AccessToken extends Model {}
 AccessToken.init({
   access_token_id: {
-    type: DataTypes.BIGINT,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
     primaryKey: true,
-    autoIncrement: true,
   },
   access_token: DataTypes.STRING(255),
   expires: DataTypes.DATE,
 }, { sequelize, modelName: 'AccessToken' });
-
-
 
 export default AccessToken;
