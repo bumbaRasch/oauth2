358a272cbea56dc02bf140ef17e934bebb48e949
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-28 00:46:14 +0100

    Delegate password generation to OAuth2 server

--- routes/client_routes.js
+++ routes/client_routes.js
@@ -1,7 +1,7 @@
 // routes/client_routes.js
 import express from 'express';
 import {client_controller} from '../controllers/client_controller.js';
-import { company_service } from '../services/company_service.js';
+import crypto from 'crypto';
 const router = express.Router();
 
 router.get('/register/client', client_controller.register_client);
@@ -10,5 +10,10 @@ router.get('/clients', client_controller.get_all_clients);
 router.get('/clients/:client_id', client_controller.get_one_client);
 router.put('/clients/:client_id', client_controller.update_client);
 router.delete('/clients/:client_id', client_controller.delete_client);
+
+router.post('/generate_secret', (req, res) => {
+    const client_secret = crypto.randomBytes(20).toString('hex');
+    res.json({ client_secret });
+});
 
 export default router;
