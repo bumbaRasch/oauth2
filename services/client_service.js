551bf254895f3c55d2d760f73355dfc0e90e1efe
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-28 22:44:07 +0100

    Added transaction handling

--- services/client_service.js
+++ services/client_service.js
@@ -4,9 +4,11 @@ import Client from "../models/Client.js";
 import Company from "../models/Company.js";
 import { generate_random_string } from "../utils/generate.js";
 import company_service_helpers from "../helpers/company_service_helpers.js";
+import sequelize  from '../models/sequelize.js';
 
 export const client_service = {
     create_client: async ( name, redirect_uri, grant_types, scope, active, company_name, company_id ) => {
+        const transaction = await sequelize.transaction();
         try {
             // Check if company_id is provided
             if (!company_id) {
@@ -22,18 +24,20 @@ export const client_service = {
                 throw new Error('Company name does not match the provided company_id');
             }
             // Check if a client with the same name or redirect_uri already exists
-            const existing_сlient = await Client.findOne({ where: { company_id, [Op.or]: [{ name }, { redirect_uri }] } });
-            if (existing_сlient) {
+            const existing_client = await Client.findOne({ where: { company_id, [Op.or]: [{ name }, { redirect_uri }] } });
+            if (existing_client) {
                 throw new Error('A client with the same name or redirect_uri already exists for this company');
             }
 
             const password = generate_random_string(16);
             const client_secret = await company_service_helpers.hash_password(password);
             const client = await Client.create({ client_secret, company_id, name, redirect_uri, grant_types, scope, active, last_active: new Date() });
-            await client.save();
+            await client.save({ transaction });
+            await transaction.commit();
             return { client_id: client.client_id, client_secret: client.client_secret, company_id: client.company_id, name: client.name, redirect_uri: client.redirect_uri, grant_types: client.grant_types, scope: client.scope, active: client.active };
         }
         catch (error) {
+            await transaction.rollback();
             throw new Error('Error creating client: ' + error.message);
         }
     },
