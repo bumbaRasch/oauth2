cd982313ed9c3454c843411293a2ae885208fe58
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-29 14:59:13 +0100

    Updated paths for client and user routes

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,11 @@ import company_routes from './company_routes.js';
 import auth_routes from './auth_routes.js';
 import home_router from './home_router.js';
 
-router.use(user_routes);
+
 // router.use('/', error_routes);
 router.use('/protected', error_routes);
-router.use(client_routes);
+router.use('/views/', client_routes);
+router.use('/views', user_routes);
 router.use(company_routes);
 router.use(auth_routes); 
 router.use(home_router);
