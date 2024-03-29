f47a557815550de4b0b6abc57840e71d65f57ff5
Dimitri Pisarev <info@dimitripisarev.com>
2024-03-29 22:35:55 +0100

    Update path for user_routes in main router

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -8,12 +8,12 @@ import { auth_middleware } from '../middlewares/auth_middleware.js';
 const router = express.Router();
 
 
-router.get('/oidc/register', user_controller.get_user_register);
-router.post('/oidc/register', validation_middleware.validate_registration_input ,user_controller.register);
-router.get('/oidc/login', user_controller.get_user_login);
-router.post('/oidc/login', user_controller.login)
-router.get('/oidc/consent', auth_middleware.authenticate_session, user_controller.get_consent);
-router.post('/oidc/consent', auth_middleware.authenticate_session, user_controller.post_consent);
+router.get('/views/oidc/register', user_controller.get_user_register);
+router.post('/views/oidc/register', validation_middleware.validate_registration_input ,user_controller.register);
+router.get('/views/oidc/login', user_controller.get_user_login);
+router.post('/views/oidc/login', user_controller.login)
+router.get('/views/oidc/consent', auth_middleware.authenticate_session, user_controller.get_consent);
+router.post('/views/oidc/consent', auth_middleware.authenticate_session, user_controller.post_consent);
 router.get('/callback', auth_middleware.authenticate_session, token_controller.exchange_code_for_token);
 
 
