c14d5dd3df79c7a02fec8eef59f0314252318df9
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-01 00:39:13 +0200

    Removed unused imports from auth_routes.js

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -1,9 +1,7 @@
 // routes/auth_routes.js
 import express from 'express';
 import { auth_controller } from '../controllers/auth_controller.js';
-import { user_controller } from '../controllers/user_controller.js';
 import { token_controller } from '../controllers/token/token_controller.js'; // import the token controller
-import { validation_middleware } from '../middlewares/validation_middleware.js';
 import { auth_middleware } from '../middlewares/auth_middleware.js';
 
 
