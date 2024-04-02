cf316f1cdedb0e2e3d3d56f50d788192bcc3901e
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-02 19:43:54 +0200

    Expand client routes and integrate token verification

--- routes/api/client_router.js
+++ routes/api/client_router.js
@@ -1,11 +1,16 @@
 // routes/api/client_router.js
 import express from 'express';
 import { client_controller } from '../../controllers/api/client_controller.js';
-import { token_controller } from '../../controllers/token/token_controller.js';
+import { token_controller } from '../../controllers/api/token_controller.js';
 
 const router = express.Router();
 
-router.post('/register/client', token_controller.verify_token, client_controller.create_client);
+router.get('/register/client', token_controller.get_temp_token);
+router.post('/register/client', token_controller.verify_token_with_next, client_controller.create_client);
+router.get('/clients', token_controller.verify_token_with_next, client_controller.get_all_clients);
+router.get('/clients/:client_id', token_controller.verify_token_with_next, client_controller.get_one_client);
+router.put('/clients/:client_id', token_controller.verify_token_with_next, client_controller.update_client);
+router.delete('/clients/:client_id', token_controller.verify_token_with_next, client_controller.delete_client);
 
 
 export default router;
