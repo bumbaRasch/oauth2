f50185a5677ce4eed5d1098be39caa1a3893bb1b
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-02 19:45:10 +0200

    Create OAuth2 route for token verification

---
+++ routes/api/oauth2_router.js
@@ -0,0 +1,9 @@
+// routes/api/aouth2.js
+import express from 'express';
+import { token_controller } from '../../controllers/api/token_controller.js';
+
+const router = express.Router();
+
+router.get('/oauth2/verify_token', token_controller.verify_token_without_next);
+
+export default router;
