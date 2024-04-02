21155d74cd5617ff60a9eb3f176befed929e16b7
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-02 19:44:42 +0200

    Integrate OAuth2 routes into API routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@
 import express from 'express';
 import { client_controller } from '../../controllers/api/client_controller.js';
 import api_client_router from '../../routes/api/client_router.js';
+import api_oauth2_router from '../../routes/api/oauth2_router.js';
 
 const router = express.Router();
 
 router.use(api_client_router);
+router.use(api_oauth2_router);
 
 export default router;
