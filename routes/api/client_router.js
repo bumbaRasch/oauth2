945d7566d23d3d50225da41476337dba4647cb47
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-04 01:01:36 +0200

    Refactored client_router.js. Updated verify_token_with_next method calls to use a key parameter.

--- routes/api/client_router.js
+++ routes/api/client_router.js
@@ -6,11 +6,11 @@ import { token_controller } from '../../controllers/api/token_controller.js';
 const router = express.Router();
 
 router.get('/register/client', token_controller.get_temp_token);
-router.post('/register/client', token_controller.verify_token_with_next, client_controller.create_client);
-router.get('/clients', token_controller.verify_token_with_next, client_controller.get_all_clients);
-router.get('/clients/:client_id', token_controller.verify_token_with_next, client_controller.get_one_client);
-router.put('/clients/:client_id', token_controller.verify_token_with_next, client_controller.update_client);
-router.delete('/clients/:client_id', token_controller.verify_token_with_next, client_controller.delete_client);
 
+router.post('/register/client', token_controller.verify_token_with_next(), client_controller.create_client);
+router.get('/clients', token_controller.verify_token_with_next('client_id'), client_controller.get_all_clients);
+router.get('/clients/:client_id', token_controller.verify_token_with_next('client_id'), client_controller.get_one_client);
+router.put('/clients/:client_id',token_controller.verify_token_with_next('client_id'), client_controller.update_client);
+router.delete('/clients/:client_id', token_controller.verify_token_with_next('client_id'), client_controller.delete_client);
 
 export default router;
