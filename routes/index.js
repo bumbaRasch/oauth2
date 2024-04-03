13273e49e57ab5bf03015473476d75582a67021b
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-04 00:59:46 +0200

    Refactored routes in index.js. Replaced company_routes with company_router and updated its usage.

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,14 @@ const router = express.Router();
 import user_routes from './user_routes.js';
 import error_routes from './error_routes.js';
 import client_routes from './client_routes.js';
-import company_routes from './company_routes.js';
+import company_router from './views/company_router.js';
 import auth_routes from './auth_routes.js';
 import home_router from './home_router.js';
 
-
-// router.use('/', error_routes);
 router.use('/protected', error_routes);
 router.use('/views/', client_routes);
 router.use('/views/', user_routes);
-router.use(company_routes);
+router.use('/views/', company_router);
 router.use(auth_routes); 
 router.use(home_router);
 
