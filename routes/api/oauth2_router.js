d6512dde08e1748d3d13bc588d4940b980451f31
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-04 01:02:45 +0200

    Updated oauth2_router.js. Added a new route for get_temp_token.

--- routes/api/oauth2_router.js
+++ routes/api/oauth2_router.js
@@ -5,5 +5,6 @@ import { token_controller } from '../../controllers/api/token_controller.js';
 const router = express.Router();
 
 router.get('/oauth2/verify_token', token_controller.verify_token_without_next);
+router.post('/oauth2/get_temp_token', token_controller.get_temp_token)
 
 export default router;
