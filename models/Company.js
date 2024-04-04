a95e5ef5106f24c218622776b006186b89667741
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-04 15:53:41 +0200

    Add status field to Company model. This change allows us to track the confirmation status of each company.

--- models/Company.js
+++ models/Company.js
@@ -6,50 +6,55 @@ import crypto from 'crypto';
 
 class Company extends Model {
   async valid_password(password) {
-    return bcrypt.compare(password, this.password);
+      return bcrypt.compare(password, this.password);
   }
 }
 
 Company.init({
   company_id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    allowNull: false,
-    defaultValue: DataTypes.UUIDV4
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4
   },
   name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: false,
   },
   email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
   },
   password: {
-    type: DataTypes.STRING,
-    allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
   },
   website: {
-    type: DataTypes.STRING,
-    allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
   },
   secret_key: {
-    type: DataTypes.STRING,
-    defaultValue: () => crypto.randomBytes(16).toString('hex') // Generate a random secret key
+      type: DataTypes.STRING,
+      defaultValue: () => crypto.randomBytes(16).toString('hex') // Generate a random secret key
   },
   updated_by: {
-    type: DataTypes.UUID,
-    allowNull: true
+      type: DataTypes.UUID,
+      allowNull: true
   },
   deleted_by: {
-    type: DataTypes.UUID,
-    allowNull: true
+      type: DataTypes.UUID,
+      allowNull: true
   },
   last_active: {
-    type: DataTypes.DATE,
-    allowNull: true,
+      type: DataTypes.DATE,
+      allowNull: true,
+  },
+  status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'unconfirmed'
   },
 }, { 
   sequelize, 
@@ -59,18 +64,18 @@ Company.init({
   updatedAt: 'updated_at',
   paranoid: true,
   hooks: {
-    beforeCreate: async (company) => {
-      if (company.changed('password')) {
-        const salt = await bcrypt.genSalt(10);
-        company.password = await bcrypt.hash(company.password, salt);
+      beforeCreate: async (company) => {
+          if (company.changed('password')) {
+              const salt = await bcrypt.genSalt(10);
+              company.password = await bcrypt.hash(company.password, salt);
+          }
+      },
+      beforeUpdate: async (company) => {
+          if (company.changed('password')) {
+              const salt = await bcrypt.genSalt(10);
+              company.password = await bcrypt.hash(company.password, salt);
+          }
       }
-    },
-    beforeUpdate: async (company) => {
-      if (company.changed('password')) {
-        const salt = await bcrypt.genSalt(10);
-        company.password = await bcrypt.hash(company.password, salt);
-      }
-    }
   }
 });
 
