467b2a86a62862cf67821f0274f32584bbf84966
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-06 23:07:32 +0200

    Added get all clients route to app routes

--- routes/api/client_router.js
+++ routes/api/client_router.js
@@ -18,5 +18,6 @@ router.delete('/clients/:client_id', token_controller.verify_token_with_next('cl
 //////////APP ROUTES//////////
 router.post('/app/register', token_controller.get_token);
 router.post('/app/create', client_controller.create_client)
+router.get('/app/clients', client_controller.get_all_clients);
 
 export default router;
