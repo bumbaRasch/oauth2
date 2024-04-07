8452a0d02b4881a0010a654a47783d898cefc89f
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-08 00:35:53 +0200

    Update client router: add confirm_temporary_client route and update get_all_clients route

--- routes/api/client_router.js
+++ routes/api/client_router.js
@@ -18,6 +18,7 @@ router.delete('/clients/:client_id', token_controller.verify_token_with_next('cl
 //////////APP ROUTES//////////
 router.post('/app/register', token_controller.get_token);
 router.post('/app/create_temp_client', token_controller.verify_token_with_next("company_id"), client_controller.create_temporary_client)
-router.get('/app/clients', client_controller.get_all_clients);
+router.post('/app/confirm_temporary_client', token_controller.verify_token_with_next("client_id"), client_controller.confirm_temporary_client)
+router.get('/app/clients', token_controller.verify_token_with_next("client_id"), client_controller.get_all_clients);
 
 export default router;
