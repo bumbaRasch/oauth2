12c58cbf6ee7770104301416162de00f9c38bb7b
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-07 12:09:28 +0200

    Update Client and Company models
    - Added status field as ENUM in Client model with values 'unconfirmed', 'confirmed', 'suspended', 'deleted'.
    - Updated status field in Company model to ENUM with values 'unconfirmed', 'confirmed', 'suspended', 'deleted'.

--- models/Client.js
+++ models/Client.js
@@ -6,116 +6,123 @@ import sequelize from './sequelize.js'; // import your sequelize instance
 class Client extends Model {}
 
 Client.init({
-  client_id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    allowNull: false,
-    unique: true,
-    defaultValue: DataTypes.UUIDV4
-  },
-  client_secret: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  redirect_uri: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  grant_types: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    get() {
-      const value = this.getDataValue('grant_types');
-      return value.includes(',') ? value.split(',') : value;
+    client_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        defaultValue: DataTypes.UUIDV4
     },
-    set(val) {
-      if (Array.isArray(val)) {
-        this.setDataValue('grant_types', val.join(','));
-      } else {
-        this.setDataValue('grant_types', val);
-      }
+    client_secret: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-  },
-  scope: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    get() {
-      const value = this.getDataValue('scope');
-      return value.includes(',') ? value.split(',') : value;
+    redirect_uri: {
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    set(val) {
-      if (Array.isArray(val)) {
-        this.setDataValue('scope', val.join(','));
-      } else {
-        this.setDataValue('scope', val);
-      }
+    grant_types: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        get() {
+            const value = this.getDataValue('grant_types');
+            return value.includes(',') ? value.split(',') : value;
+        },
+        set(val) {
+            if (Array.isArray(val)) {
+                this.setDataValue('grant_types', val.join(','));
+            } 
+            else {
+                this.setDataValue('grant_types', val);
+            }
+        },
     },
-  },
-  company_id: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
-  active: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  website: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  logo: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  privacy_policy: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  terms_of_service: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  },
-  last_active: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-}, {
-  sequelize,
-  modelName: 'Client',
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  hooks: {
-    beforeCreate: async (client) => {
-      if (client.changed('client_secret')) {
-        const salt = await bcrypt.genSalt(10);
-        client.client_secret = await bcrypt.hash(client.client_secret, salt);
-      }
+    scope: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        get() {
+            const value = this.getDataValue('scope');
+            return value.includes(',') ? value.split(',') : value;
+        },
+        set(val) {
+            if (Array.isArray(val)) {
+                this.setDataValue('scope', val.join(','));
+            } 
+            else {
+                this.setDataValue('scope', val);
+            }
+        },
     },
-    beforeUpdate: async (client) => {
-      if (client.changed('client_secret')) {
-        const salt = await bcrypt.genSalt(10);
-        client.client_secret = await bcrypt.hash(client.client_secret, salt);
-      }
+    company_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+    },
+    active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+    website: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    logo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    privacy_policy: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    terms_of_service: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    last_active: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.ENUM,
+        values: ['unconfirmed', 'confirmed', 'suspended', 'deleted'],
+        defaultValue: 'unconfirmed'
+    },
+    }, {
+        sequelize,
+        modelName: 'Client',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        hooks: {
+        beforeCreate: async (client) => {
+            if (client.changed('client_secret')) {
+                const salt = await bcrypt.genSalt(10);
+                client.client_secret = await bcrypt.hash(client.client_secret, salt);
+            }
+        },
+        beforeUpdate: async (client) => {
+            if (client.changed('client_secret')) {
+                const salt = await bcrypt.genSalt(10);
+                client.client_secret = await bcrypt.hash(client.client_secret, salt);
+            }
+        }
     }
-  }
-  
+    
 });
 
 export default Client;
--- models/Company.js
+++ models/Company.js
@@ -5,78 +5,78 @@ import bcrypt from 'bcryptjs';
 import crypto from 'crypto';
 
 class Company extends Model {
-  async valid_password(password) {
-      return bcrypt.compare(password, this.password);
-  }
+    async valid_password(password) {
+        return bcrypt.compare(password, this.password);
+    }
 }
 
 Company.init({
-  company_id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      allowNull: false,
-      defaultValue: DataTypes.UUIDV4
-  },
-  name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: false,
-  },
-  email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-  },
-  password: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-  website: {
-      type: DataTypes.STRING,
-      allowNull: false
-  },
-  secret_key: {
-      type: DataTypes.STRING,
-      defaultValue: () => crypto.randomBytes(16).toString('hex') // Generate a random secret key
-  },
-  updated_by: {
-      type: DataTypes.UUID,
-      allowNull: true
-  },
-  deleted_by: {
-      type: DataTypes.UUID,
-      allowNull: true
-  },
-  last_active: {
-      type: DataTypes.DATE,
-      allowNull: true,
-  },
-  status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: 'unconfirmed'
-  },
+    company_id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: DataTypes.UUIDV4
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    website: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    secret_key: {
+        type: DataTypes.STRING,
+        defaultValue: () => crypto.randomBytes(16).toString('hex') // Generate a random secret key
+    },
+    updated_by: {
+        type: DataTypes.UUID,
+        allowNull: true
+    },
+    deleted_by: {
+        type: DataTypes.UUID,
+        allowNull: true
+    },
+    last_active: {
+        type: DataTypes.DATE,
+        allowNull: true,
+    },
+    status: {
+        type: DataTypes.ENUM,
+        values: ['unconfirmed', 'confirmed', 'suspended', 'deleted'],
+        defaultValue: 'unconfirmed'
+    },
 }, { 
-  sequelize, 
-  modelName: 'Company',
-  timestamps: true,
-  createdAt: 'created_at',
-  updatedAt: 'updated_at',
-  paranoid: true,
-  hooks: {
-      beforeCreate: async (company) => {
-          if (company.changed('password')) {
-              const salt = await bcrypt.genSalt(10);
-              company.password = await bcrypt.hash(company.password, salt);
-          }
-      },
-      beforeUpdate: async (company) => {
-          if (company.changed('password')) {
-              const salt = await bcrypt.genSalt(10);
-              company.password = await bcrypt.hash(company.password, salt);
-          }
-      }
-  }
+    sequelize, 
+    modelName: 'Company',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+    paranoid: true,
+    hooks: {
+        beforeCreate: async (company) => {
+            if (company.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                company.password = await bcrypt.hash(company.password, salt);
+            }
+        },
+        beforeUpdate: async (company) => {
+            if (company.changed('password')) {
+                const salt = await bcrypt.genSalt(10);
+                company.password = await bcrypt.hash(company.password, salt);
+            }
+        }
+    }
 });
 
 export default Company;
