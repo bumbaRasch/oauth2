db4b46040182f905f510e302c10714a62375d13d
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-08 00:32:24 +0200

    Update oauth2 router. Add routes for getting tokens for companies and clients.

--- routes/api/oauth2_router.js
+++ routes/api/oauth2_router.js
@@ -1,10 +1,12 @@
 // routes/api/aouth2.js
 import express from 'express';
 import { token_controller } from '../../controllers/api/token_controller.js';
+import validators from '../../utils/validators.js';
 
 const router = express.Router();
 
 router.get('/oauth2/verify_token', token_controller.verify_token_without_next);
 router.post('/oauth2/get_token', token_controller.get_token)
-
+router.post('/oauth2/companies/get_token', validators.validate_company_token_request, token_controller.get_token)
+router.post('/oauth2/clients/get_token',  token_controller.get_token)
 export default router;
