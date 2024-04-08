4d077d46211acf5dfca306fdb73412205f6a3ff9
Dimitri Pisarev <info@dimitripisarev.com>
2024-04-08 22:46:05 +0200

    Updated oauth2 routes to include client token validation

--- routes/api/oauth2_router.js
+++ routes/api/oauth2_router.js
@@ -2,11 +2,12 @@
 import express from 'express';
 import { token_controller } from '../../controllers/api/token_controller.js';
 import validators from '../../utils/validators.js';
+import { client_controller } from '../../controllers/api/client_controller.js';
 
 const router = express.Router();
 
 router.get('/oauth2/verify_token', token_controller.verify_token_without_next);
 router.post('/oauth2/get_token', token_controller.get_token)
-router.post('/oauth2/companies/get_token', validators.validate_company_token_request, token_controller.get_token)
-router.post('/oauth2/clients/get_token',  token_controller.get_token)
+router.post('/oauth2/companies/token', validators.validate_company_token_request, token_controller.get_token)
+router.post('/oauth2/clients/token', validators.validate_client_token_request, token_controller.get_token)
 export default router;
